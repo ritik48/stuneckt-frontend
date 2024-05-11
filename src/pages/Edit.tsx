2461a5d1be46dce3df14dff429e8e83fa5577616
Ritik Raj <raj769417@gmail.com>
2024-05-12 03:35:06 +0530

    fix: render the edit page only when user is logged in

--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -7,15 +7,15 @@ import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-// import { userLogin } from "../apis";
 import toast from "react-hot-toast";
 import { useUser } from "../contexts/UserContext";
+import { Navigate } from "react-router-dom";
 
 export function Edit() {
-    const { update, user } = useUser();
+    const { update, user, loading } = useUser();
 
-    const [username, setUsername] = React.useState<string | null>(
-        user ? user.username : null
+    const [username, setUsername] = React.useState<string>(
+        user ? user.username : ""
     );
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -32,51 +32,57 @@ export function Edit() {
         await update({ username });
     };
 
+    if (!loading && !user) {
+        return <Navigate to={"/"} />;
+    }
+
     return (
-        <Container component="main" maxWidth="xs">
-            <CssBaseline />
-            <Box
-                sx={{
-                    marginTop: 8,
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                }}
-            >
-                <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                    Update username
-                </Typography>
+        !loading && (
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
                 <Box
-                    component="form"
-                    onSubmit={handleSubmit}
-                    noValidate
-                    sx={{ mt: 1 }}
+                    sx={{
+                        marginTop: 8,
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
                 >
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="username"
-                        label="Username"
-                        name="username"
-                        value={username}
-                        autoComplete="username"
-                        autoFocus
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        sx={{ mt: 3, mb: 2 }}
+                    <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Update username
+                    </Typography>
+                    <Box
+                        component="form"
+                        onSubmit={handleSubmit}
+                        noValidate
+                        sx={{ mt: 1 }}
                     >
-                        Update
-                    </Button>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="username"
+                            label="Username"
+                            name="username"
+                            value={username}
+                            autoComplete="username"
+                            autoFocus
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Update
+                        </Button>
+                    </Box>
                 </Box>
-            </Box>
-        </Container>
+            </Container>
+        )
     );
 }
